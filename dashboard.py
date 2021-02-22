fae4855c9788067543389aea42af053eb94dc5c6
Robert <rtedwards.eng@gmail.com>
2021-02-22 00:30:37 +0000

    sending x,y from source to workers to sink

---
+++ dashboard.py
@@ -0,0 +1,97 @@
+from datetime import datetime, timedelta
+import time
+import random
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
+import zmq
+
+class SinkPage:
+    def __init__(self):
+        """[summary]
+
+        Args:
+            port (str): port to connect to
+            topic (str): topic to listen to
+        """
+        self.colors = {"blue": "#5276A7", "green": "#57A44C", "red": "#8B0000"}
+
+        st.set_page_config(
+            page_title="Pipeline",
+            page_icon="⏳",
+            layout="wide",
+            initial_sidebar_state="expanded",
+        )
+
+        self.df = pd.DataFrame({"x": [0.0], "y": [0.0], "color": ["#8B0000"], "n": [0]})
+    
+        self.context: zmq.sugar.context.Context
+        self.sink: zmq.sugar.socket.Socket
+        
+    def display(self):
+        st.title("Sink")
+        # ---- USER INPUTS ---- #
+        port = st.sidebar.number_input(label="Port:", value=5558)
+
+        # ---- PAGE LAYOUT ---- #
+        # col1 displays charts
+        # col2 displays live messages comming in with newest at top
+        col1, col2 = st.beta_columns([2, 1])
+        approx = col1.empty()
+        num_messages = col2.empty()
+        approx.subheader(f"π ≈ -")
+        num_messages.subheader(f"Messages: ")
+
+        chart1 = col1.empty()
+        data_feed = col2.empty()
+
+        plot = alt.Chart(self.df, height=500, width=500).mark_circle(opacity=1, size=5).encode(
+                x=alt.X('x', axis=None),
+                y=alt.Y('y', axis=None),
+                color=alt.Color('color', legend=None)
+            )
+        chart1.altair_chart(plot)
+
+        # ---- SETUP SOCKET ---- #
+        self.setup_socket(port)
+
+        num_in = 0
+        num_total = 0
+        while True:
+            x, y = self.receive_data()
+
+            color = self.colors["red"] if (x**2 + y**2) <= 1 else self.colors["blue"] 
+            num_total += 1
+            num_in += 1 if color == self.colors["red"] else 0
+            df = pd.DataFrame({"x": [x], "y": [y], "color": [color], "n": [num_total]})
+            self.df = pd.concat([df, self.df])
+            self.df.set_index("n")
+
+            pi = 4 * num_in / num_total
+
+            approx.subheader(f"π ≈ {pi}")
+            num_messages.subheader(f"Messages: {num_total}")
+            chart1.add_rows(df)
+            
+            # data_feed.write(self.df)
+
+            
+    def receive_data(self) -> pd.DataFrame:
+        # TODO wrap in method
+        msg = self.sink.recv().decode('utf-8')
+        msg = str(msg)
+        x, y = msg.split(",")
+        return ( float(x), float(y) )
+
+    def setup_socket(self, port: str):
+        self.context = zmq.Context()
+
+        # Socket to receive messages on
+        self.sink = self.context.socket(zmq.PULL)
+        self.sink.bind(f"tcp://*:{port}")
+
+if __name__ == "__main__":
+    SinkPage().display()
+
---
+++ source.py
@@ -0,0 +1,75 @@
+import random
+import time
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+import zmq
+
+class SourcePage:
+
+    def __init__(self):
+
+        # Initialize random number generator
+        random.seed()
+
+        self.context: zmq.sugar.context.Context
+        self.vent: zmq.sugar.socket.Socket
+
+    def display(self):
+
+        st.title("Ventillator")
+
+        # ---- USER INPUTS ---- #
+        port = st.sidebar.number_input(label="Port:", value=5557)
+        n_points = st.sidebar.number_input(label="N points:", min_value=0, value=1000)
+        button = st.sidebar.empty()
+
+        # ---- SETUP SOCKET ---- #
+        self.setup_socket(port)
+
+        # ---- PROCESS ---- #
+        x = np.random.uniform(-1,1, n_points)
+        y = np.random.uniform(-1,1, n_points)
+        data = np.array([x, y]).T
+
+        # ---- SEND DATA ---- #
+        if button.button(label="Send Data"):
+            with st.spinner("Sending data..."):
+                try:
+                    self.send_data(data)
+                except KeyboardInterrupt as k:
+                    st.info(k)
+                finally:
+                    # clean up
+                    self.vent.close()
+                    self.context.term()
+                    pass
+
+    def send_data(self, data):
+        bar = st.progress(0)
+        total = len(data)
+        for i, d in enumerate(data):
+            msg_string = f"{d[0]},{d[1]}"
+            msg_bytes = msg_string.encode('UTF-8')
+
+            self.vent.send(msg_bytes)
+            bar.progress(i / total)
+        
+    def setup_socket(self, port: str):
+        # try:
+        #     self.vent.close()
+        #     self.context.term()
+        # except Exception as e:
+        #     # st.sidebar.info("Sockets closed")
+        #     # st.write(e)
+        #     pass
+
+        self.context = zmq.Context()
+
+        # Socket to send messages on
+        self.vent = self.context.socket(zmq.PUSH)
+        self.vent.bind(f"tcp://*:{port}")
+
+if __name__ == "__main__":
+    SourcePage().display()
